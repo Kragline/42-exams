5f3ed5c166a9ff37a2104194cfa0cd690ee65a24
Arman Arakelyan <armarake@c2r3s2.42yerevan.am>
2025-12-04 15:03:58 +0400

    simpler game_of_life

--- rang05-new/level-2/game_of_life/main.c
+++ rang05-new/level-2/game_of_life/main.c
@@ -1,30 +1,114 @@
-#include "life.h"
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
 
-//clear && gcc main.c life.c life.h && echo 'sdxddssaaww' | valgrind --leak-check=full --show-leak-kinds=all ./a.out 5 5 0 && rm ./a.out life.h.gch
+void print_board(char **new_board, int height, int width)
+{
+	for (int row = 0; row < height; row++)
+    {
+		for (int col = 0; col < width; col++)
+            putchar(new_board[row][col]);
+        putchar('\n');
+    }
+}
 
-//clear && gcc main.c life.c life.h && echo 'sdxssdswdxdddxsaddawxwdxwaa' | valgrind --leak-checkfull --show-leak-kinds=all ./a.out 10 6 0 && rm ./a.out life.h.gch
+void free_board(char **board, int height)
+{
+    for (int row = 0; row < height; row++)
+        free(board[row]);
+    free(board);
+}
 
-//clear && gcc main.c life.c life.h && echo 'dxss' | valgrind --leak-check=full --show-leak-kinds=all ./a.out 3 3 1 && rm ./a.out life.h.gch
+char **start_board(char **new_board, int width, int height)
+{
+    char c;
+    int index = 0;
+    int array = 0;
+    int drawing = 0;
+    while (read(STDIN_FILENO, &c, 1) > 0)
+    {
+		if (c == 'x')
+			drawing = !drawing;
+		else if (c == 'w' && array > 0)
+			array--;
+		else if (c == 'a' && index > 0)
+			index--;
+		else if (c == 's' && (array + 1 < height))
+			array++;
+		else if (c == 'd' && (index + 1 < width))
+			index++;
+		if (drawing)
+			new_board[array][index] = '0';
+    }
+	return (new_board);
+}
 
-/*
-Plan:
-1) get the 3 values immediately
+char **board_creation(int width, int height)
+{
+	char **new_board = calloc(height, sizeof(char *));
+	for (int row = 0; row < height; row++)
+	{
+		new_board[row] = calloc(width, sizeof(char));	
+		for (int col = 0; col < width; col++)
+			new_board[row][col] = ' ';
+	}
+	return (new_board);
+}
 
-2) create an empty board with calloc, filled with spaces ' '.
-3) use Read to get the location of living cells and add them in
+int count_neighbors(char **board, int row, int col, int width, int height)
+{
+	int count = 0;
+	for (int dr = -1; dr <= 1; dr++)
+	{
+		for (int dc = -1; dc <= 1; dc++)
+		{
+			if (dr == 0 && dc == 0)
+				continue;
+			int nr = row + dr;
+			int nc = col + dc;
+			if (nr >= 0 && nr < height && nc >= 0 && nc < width)
+				if (board[nr][nc] == '0')
+					count++;
+		}
+	}
+	return (count);
+}
 
-4) if (iteration > 0), get the new_board and get the '8' neighbours.
-5) update the board and free the boards you aren't using
+char **next_gen(char **board, int width, int height)
+{
+	char **new_board = board_creation(width, height);
+	for (int row = 0; row < height; row++)
+	{
+		for (int col = 0; col < width; col++)
+		{
+			int neighbors = count_neighbors(board, row, col, width, height);
+			if (board[row][col] == '0')
+			{
+				if (neighbors == 2 || neighbors == 3)
+					new_board[row][col] = '0';
+			}
+			else
+				if (neighbors == 3)
+					new_board[row][col] = '0';
+		}
+	}
+	free_board(board, height);
+	return (new_board);
+}
 
-6) ensure you have a print board and a free board.
-
-Finally,
-free everything ensure nothing is left. Check valgrind
-*/
-
-int	main(int argc, char **argv)
+int main (int argc, char **argv)
 {
 	if (argc == 4)
-		game_of_life_start(argv);//convert the char into size_t
+	{
+		int width = atoi(argv[1]);
+		int height = atoi(argv[2]);
+		int max_iterations = atoi(argv[3]);
+		char **empty_board = board_creation(width, height);
+		char **board = start_board(empty_board, width, height);
+		while (max_iterations--)
+			board = next_gen(board, width, height);
+		print_board(board, height, width);
+		free_board(board, height);
+	}
 	return (0);
 }
